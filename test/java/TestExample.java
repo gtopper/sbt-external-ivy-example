a822a3ef027c71620c547b55287cd42715a4f978
Gal Topper <gal@liveramp.com>
2013-08-17 14:56:27 -0700

    Fix source.

--- test/java/TestExample.java
+++ test/java/TestExample.java
@@ -2,5 +2,5 @@ import org.apache.commons.cli.BasicParser;
 
 public class TestExample {
   public static final BasicParser a = new BasicParser();
-  public static final Example a = new Example();
+  public static final Example b = new Example();
 }
